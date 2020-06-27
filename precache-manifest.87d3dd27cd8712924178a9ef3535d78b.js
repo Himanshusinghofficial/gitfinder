1ce897649ae48f0559a1799a88281640fc8f6690
Himanshusinghofficial <hs882677@gmail.com>
2020-06-28 02:53:10 +0530

    Updates

---
+++ precache-manifest.87d3dd27cd8712924178a9ef3535d78b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0742829f858137e93c806fd0ba67d0",
+    "url": "/gitfinder/index.html"
+  },
+  {
+    "revision": "bb7b00d1dd64446f1a20",
+    "url": "/gitfinder/static/css/main.bc1a1419.chunk.css"
+  },
+  {
+    "revision": "06dbda87aef7b899d84f",
+    "url": "/gitfinder/static/js/2.a8bd8f94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gitfinder/static/js/2.a8bd8f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb7b00d1dd64446f1a20",
+    "url": "/gitfinder/static/js/main.e3c97547.chunk.js"
+  },
+  {
+    "revision": "647ef1cead0b853497e2",
+    "url": "/gitfinder/static/js/runtime-main.6b249eeb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c1beae49b8da8d47e11b7f5654cf011.js"
+  "/gitfinder/precache-manifest.87d3dd27cd8712924178a9ef3535d78b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gitfinder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
